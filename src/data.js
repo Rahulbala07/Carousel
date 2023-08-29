04e6c1c7c9eee78dca135191c0c493ec902bdc12
Rahulbala07 <romeorahul286@gmail.com>
2023-08-29 09:52:28 +0530

    code pushing

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const dataDigitalBestSeller = [
+    {
+      id: 1,
+      title: 'Kang Daniel:My Parade ',
+      price: 'Korean',
+      category: 'Music,Documentary',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/KANGDANIEL--MY-PARADE--poster-Recovered-e406b170-360e-11ee-a3fe-6d0a80a1228c.jpg?format=webp&imwidth=100%',
+    },
+    {
+      id: 2,
+      title: 'Angaaragan',
+      price: 'Tamil',
+      category: 'Thriller,Horror',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/Angaaragan--poster-78a32d50-3bea-11ee-965d-f906ef96814c.jpg?format=webp&imwidth=100%',
+    },
+    {
+      id: 3,
+      title: 'Demon',
+      price: 'Tamil',
+      category: 'Horror',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/Demon-5abae020-3772-11ee-8b1c-c54a903cd027.jpg?format=webp&imwidth=100%https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/p/pokemon-legends-arceus-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 4,
+      title: 'Finder',
+      price: 'Tamil,Hindi,Malayalam',
+      category: 'Suspense,Thriller',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/finder-poster-57c1b040-3765-11ee-8b1c-c54a903cd027.jpg?format=webp&imwidth=100%',
+    },
+    {
+      id: 5,
+      title: 'Kick',
+      price: 'Tamil,Telugu',
+      category: 'Action,Comedy,Romance',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/Kick--poster-323e2400-3762-11ee-ac33-6710aa9bf74c.jpg?format=webp&imwidth=100%',
+    },
+    {
+      id: 6,
+      title: 'Kushi',
+      price: 'Tamil,Telugu,Kannada,Hindi,Malayalam',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/Khushi-poster-c5ef6e90-cd89-11ed-a1bf-d5df025d8cb6.jpg?format=webp&imwidth=100%',
+    },
+    {
+      id: 7,
+      title: 'Jawan',
+      price: 'Tamil,Telugu,Hindi',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/Jawan-80446e90-376f-11ee-ac33-6710aa9bf74c.jpg?format=webp&imwidth=100%',
+    },
+    {
+      id: 8,
+      title: 'Goldfish',
+      price: 'Hindi',
+      category: 'Drama',
+      sale: 63, //percent
+      linkImg:
+        'https://assetscdn1.paytm.com/images/cinema/Goldfish-poster-d271ea00-30ec-11ee-9e01-394e3ac5a059.jpg?format=webp&imwidth=100%',
+    },
+  ];
